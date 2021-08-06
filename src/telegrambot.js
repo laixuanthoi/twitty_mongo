ed645af374d1b03207e67f511415458e85a33886
laixuanthoi <laixuanthoi@gmail.com>
2021-08-06 13:52:43 +0700

    okie

--- src/telegrambot.js
+++ src/telegrambot.js
@@ -1,7 +1,8 @@
 import TelegramBot from "node-telegram-bot-api";
 
 const token = "1844525211:AAH5lzam-fffd6pLh8Li6rH8uXpS8Gh5lKU";
-const Promise = require("bluebird");
+import { Promise } from "bluebird";
+
 Promise.config({
   cancellation: true,
 });
