1da324b7a6680c328a97396646496527cbd59019
laixuanthoi <laixuanthoi@gmail.com>
2021-08-06 13:51:36 +0700

    okie

--- src/telegrambot.js
+++ src/telegrambot.js
@@ -1,6 +1,11 @@
 import TelegramBot from "node-telegram-bot-api";
 
 const token = "1844525211:AAH5lzam-fffd6pLh8Li6rH8uXpS8Gh5lKU";
+const Promise = require("bluebird");
+Promise.config({
+  cancellation: true,
+});
+
 const bot = new TelegramBot(token, { polling: true });
 
 export { bot };
