95497d57c80a6eca7f65112d2dc1c62ced9443d3
laixuanthoi <laixuanthoi@gmail.com>
2021-08-08 11:04:40 +0700

    okie

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ import axiosClient from "./src/axiosClient.js";
 
 import TelegramBot from "node-telegram-bot-api";
 
-const token = "1844525211:AAH5lzam-fffd6pLh8Li6rH8uXpS8Gh5lKU";
+// const token = "1844525211:AAH5lzam-fffd6pLh8Li6rH8uXpS8Gh5lKU";
+const token = "1936409627:AAGggXnLmHSQ8p-wgu5CFNNBfa7Sha2WCcw";
 
 const bot = new TelegramBot(token, { polling: true });
 
